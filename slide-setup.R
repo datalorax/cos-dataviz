e9ec652c963e4e938cf1466a2f2b1051dee347a4
Daniel Anderson <daniela@uoregon.edu>
2021-05-13 13:03:05 -0700

    update footer

--- slide-setup.R
+++ slide-setup.R
@@ -9,15 +9,15 @@ setup <- function(slides) {
   <div class="slides-footer">
   <span>
   
-  <a class = "footer-icon-link" href = "https://github.com/datalorax/psych-seminar21/raw/main/anderson-psych-seminar21.pdf">
+  <a class = "footer-icon-link" href = "https://github.com/datalorax/cos-dataviz/raw/main/anderson-cos-dataviz.pdf">
     <i class = "footer-icon" data-feather="download"></i>
   </a>
   
-  <a class = "footer-icon-link" href = "https://datalorax.github.io/psych-seminar21/">
+  <a class = "footer-icon-link" href = "https://datalorax.github.io/cos-dataviz">
     <i class = "footer-icon" data-feather="link"></i>
   </a>
   
-  <a class = "footer-icon-link" href = "https://github.com/datalorax/psych-seminar21">
+  <a class = "footer-icon-link" href = "https://github.com/datalorax/cos-dataviz">
     <i class = "footer-icon" data-feather="github"></i>
   </a>
   
